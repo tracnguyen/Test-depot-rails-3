b8d78735eafb6f58be06b3191ed52ee7390dc9bd
Hai-Anh Trinh <haianh.trinh@techpropulsionlabs.com>
2010-06-14 16:25:12 +0700

    Add root page

--- config/routes.rb
+++ config/routes.rb
@@ -46,9 +46,7 @@ Hiringapp::Application.routes.draw do |map|
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "welcome#index"
 
   # See how all your routes lay out with "rake routes"
 
