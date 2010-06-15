03966db567c6fab34db9cf8d3d9d5110f95115b8
Hai-Anh Trinh <haianh.trinh@techpropulsionlabs.com>
2010-06-15 10:51:32 +0700

    rails generate devise User; rails generate devise:views # please migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Hiringapp::Application.routes.draw do |map|
+  root :to => "welcome#index"
+  
+  constraints(:subdomain => /.+/) do 
+    root :to => "welcome#index"
+  end
+  
+  devise_for :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -45,14 +53,6 @@ Hiringapp::Application.routes.draw do |map|
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  constraints(:subdomain => /.+/) do 
-    root :to => "welcome#index"
-  end
-
-  root :to => "welcome#index"
-
-  # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
