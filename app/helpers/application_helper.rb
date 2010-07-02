95501938806d1d83324ef6f63a907715185032f7
Hai-Anh Trinh <haianh.trinh@techpropulsionlabs.com>
2010-07-02 13:33:30 +0700

    Fix the date range

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,17 +26,17 @@ module ApplicationHelper
     elsif d < 86400
       h = d/3600
       "#{h.to_i} hour#{h >=2 ? 's' : ''} ago"
-    elsif d < 2073600
+    elsif d < 604800
       dy = d/86400
       "#{dy.to_i} day#{dy >=2 ? 's' : ''} ago"
-    elsif d < 14515200
-      w = d/2073600
+    elsif d < 2419200
+      w = d/604800
       w <=2 ? "last week" : "#{w.to_i} weeks ago"
-    elsif d < 756864000
-       m = d/14515200
+    elsif d < 29030400 # not exactly a year, we just count a month as 30 days
+       m = d/2419200
        m <= 2 ? "last month" : "#{m.to_i} months ago"
     else
-      y = d/756864000
+      y = d/31536000
       y <= 2 ? "last year" : "#{y.to_i} years ago"
     end
   end
