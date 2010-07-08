ba03d5c4b8117c946d879fe65910ecba0a9e94a5
Hoang Tran <hoang.tran@techpropulsionlabs.com>
2010-07-07 21:56:49 -0700

    refactor stages' character color mapping

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,47 @@
 module ApplicationHelper
+  CharColorMapping = {
+    '#000000' => '#FFFFFF', 
+    '#993300' => '#FFFFFF',
+    '#333300' => '#FFFFFF', 
+    '#000080' => '#FFFFFF', 
+    '#333399' => '#FFFFFF', 
+    '#333333' => '#FFFFFF', 
+    '#800000' => '#FFFFFF', 
+    '#FF6600' => '#FFFFFF', 
+    '#808000' => '#FFFFFF', 
+    '#008000' => '#FFFFFF', 
+    '#008080' => '#FFFFFF', 
+    '#0000FF' => '#FFFFFF', 
+    '#666699' => '#FFFFFF', 
+    '#808080' => '#FFFFFF', 
+    '#FF0000' => '#FFFFFF', 
+    '#FF9900' => '#FFFFFF', 
+    '#99CC00' => '#FFFFFF', 
+    '#339966' => '#FFFFFF', 
+    '#33CCCC' => '#FFFFFF', 
+    '#3366FF' => '#FFFFFF', 
+    '#800080' => '#FFFFFF', 
+    '#999999' => '#FFFFFF', 
+    '#FF00FF' => '#FFFFFF', 
+    '#FFCC00' => '#000000', 
+    '#FFFF00' => '#000000', 
+    '#00FF00' => '#000000', 
+    '#00FFFF' => '#000000', 
+    '#00CCFF' => '#000000', 
+    '#993366' => '#000000', 
+    '#C0C0C0' => '#000000', 
+    '#FF99CC' => '#000000', 
+    '#FFCC99' => '#000000', 
+    '#FFFF99' => '#000000', 
+    '#CCFFFF' => '#000000', 
+    '#99CCFF' => '#000000', 
+    '#FFFFFF' => '#000000'
+  };
+  
+  def job_status_collection
+    [['Draft', 'draft'],['Open', 'open'],['Closed', 'closed']]
+  end
+  
   def job_filter_collection
     [["All", ""]] + current_account.jobs.collect{|job| [job.title, job.id.to_s]}
   end
@@ -35,5 +78,11 @@ module ApplicationHelper
       y = d/31536000
       y <= 2 ? "last year" : "#{y.to_i} years ago"
     end
+  end
+  
+  def map_to_char_color(background_color)
+    background_color.gsub!(/\s/, '')
+    background_color.upcase!
+    CharColorMapping[background_color]
   end
 end
