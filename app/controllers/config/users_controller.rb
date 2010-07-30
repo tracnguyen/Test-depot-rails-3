855026db8102f1c1d05e051f0588fb5698189596
Hoang Tran <hoang.tran@techpropulsionlabs.com>
2010-07-30 03:04:57 -0700

    validate user invitation

--- app/controllers/config/users_controller.rb
+++ app/controllers/config/users_controller.rb
@@ -13,12 +13,18 @@ class Config::UsersController < BaseAccountController
   
   def create
     @invitation = current_user.invitations.build(params[:user])
-    if @invitation.save && InvitationMailer.welcome_email(@invitation).deliver
-     flash[:notice] = "The Invitation has been sent."
-     redirect_to config_users_path
-   else
-     flash[:alert] = "There as an error creating the invitation."
-     render "index"
-   end
+
+    if @invitation.save 
+      if InvitationMailer.welcome_email(@invitation).deliver
+        flash[:notice] = "The Invitation has been sent."
+        redirect_to config_users_path
+      else 
+        flash[:notice] = "The Invitation sending failed."
+        redirect_to config_users_path
+      end
+    else
+      flash[:alert] = @invitation.errors.full_messages.first
+      redirect_to config_users_path
+    end 
   end
 end
--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -11,6 +11,9 @@
 class Invitation < ActiveRecord::Base
   after_create :reset_token!
   belongs_to :inviter, :class_name => "User"
+  
+  validates_presence_of :email
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
    
   def reset_token!
     self.token = ActiveSupport::SecureRandom.hex(16)
